6412b353cb80191b03e44e3fc19fb20f9ae55c7c
JaxV9 <layanj.pro@gmail.com>
2025-04-06 10:57:45 +0200

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EnvironmentConfigModule } from './infra/config/environment-config/environment-config.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infra/db/db.module';
 
 @Module({
-  imports: [EnvironmentConfigModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
